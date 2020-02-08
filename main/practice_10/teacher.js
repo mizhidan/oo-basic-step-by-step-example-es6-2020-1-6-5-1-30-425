77ee72fd6f4ea7b22550a8ae30e32d1a39a6f1d8
mizhidan <dj9824@126.com>
2020-02-08 19:10:17 +0800

    [danjie] feat: complete all

--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,20 @@
+import Person from './person.js'
 
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+
+  introduce() {
+    let klassNum;
+    if (this.klasses) {
+      klassNum = this.klasses.map((klass) => klass.number).join(', ');
+    }
+    return `${super.introduce()} I am a Teacher. I teach `
+      + (this.klasses ? `Class ${klassNum}` : 'No Class')
+      + '.';
+  }
+}
+
+module.exports = Teacher;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,33 @@
+import Person from './person.js'
 
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+
+  introduce() {
+    let klassNum;
+    if (this.klasses) {
+      klassNum = this.klasses.map((klass) => klass.number).join(', ');
+    }
+    return `${super.introduce()} I am a Teacher. I teach `
+      + (this.klasses ? 'Class ' + klassNum : 'No Class')
+      + '.';
+  }
+
+  isTeaching(student) {
+    return this.klasses.includes(student.klass);
+  }
+
+  callWithLeader(student) {
+    console.log(`I am ${this.name}. I know ${student.name} become Leader of Class ${student.klass.number}.`);
+  }
+
+  callWithJoin(student) {
+    console.log(`I am ${this.name}. I know ${student.name} has joined Class ${student.klass.number}.`);
+  }
+  
+}
+
+module.exports = Teacher;
